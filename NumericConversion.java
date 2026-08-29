f07c18329f18bf70c781ad47cd3ca3df6b92d0e3
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 00:38:51 +0800

    Create NumericConversion.java

---
+++ NumericConversion.java
@@ -0,0 +1,14 @@
+//Filename NumericConversion.java
+//Written by Diana Grace Batara
+// BSIT NETSEC 1-2
+// WRITTEN ON AUGUST 22, 2026
+
+public class NumericConversion {
+    public static void main(String[] args) {
+        int hoursWorked = 37;
+        double payRate = 4.50;
+        double grossPay = hoursWorked * payRate;
+
+        System.out.println("Gross pay is " + grossPay);
+    }
+  }
