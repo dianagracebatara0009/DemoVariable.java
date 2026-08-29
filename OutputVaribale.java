81862fe02cfa78e500d8e490f58fc0cd8ed2ee59
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 00:32:20 +0800

    Create OutputVaribale.java

---
+++ OutputVaribale.java
@@ -0,0 +1,29 @@
+//Fillname OutputVariable.java
+//Written by Diana Grace Batara
+// BSIT NETSEC 1-2
+// WRITTEN ON AUGUST 22, 2026
+
+import java.util.Scanner;
+
+public class UserInputs {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        String user_name;
+
+        System.out.print("Enter your name: ");
+        user_name = user_input.next();
+
+        System.out.println("Your name is: " + user_name);
+    }
+}
+
+public class OutputVariable {
+    public static void main(String[] args) {
+        int value = 10;
+        char x;
+        x = 'A';
+
+        System.out.println(value);
+        System.out.println("The value of x = " + x);
+    }
+}
