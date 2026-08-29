af16b0602e3ecf6ba64b6ca8b99ba14762b34965
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 00:29:22 +0800

    Create UserInput.java

---
+++ UserInput.java
@@ -0,0 +1,18 @@
+//Filname UserInouts.java
+//Written by Diana Grace Batara
+// BSIT NETSEC 1-2
+// WRITTEN ON AUGUST 22, 2026
+
+import java.util.Scanner;
+
+public class UserInputs {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        String user_name;
+
+        System.out.print("Enter your name: ");
+        user_name = user_input.next();
+
+        System.out.println("Your name is: " + user_name);
+    }
+}
