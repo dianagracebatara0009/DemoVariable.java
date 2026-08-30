063d947b6e016fe41dd44031955a6cd24ffb690e
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:43:02 +0800

    Create DemoVariables4.java

---
+++ DemoVariables4.java
@@ -0,0 +1,15 @@
+//Filename DemoVariables4.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+package helloworld.variable;
+
+public class DemoVariables4  {
+	public static void main(String[ ]  args)
+	{
+		char myGrade = 'A',  myFriendsGrade='C';
+		
+		System.out.println("Our grades are " + myGrade+ "and"+ myFriendsGrade);		
+	}
+}
