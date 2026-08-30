e9e9cdc476fab7593b783535054baee2c4d55399
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:34:37 +0800

    Create DemoVariables7.java

---
+++ DemoVariables7.java
@@ -0,0 +1,14 @@
+ //Filename DemoVariable7.java
+ //Written by Diana Grace Batara
+ //BSIT NETSEC 1-2
+ //Written August 22 ,2026
+ 
+ public class DemoVariable7 {
+   public static void main(String[ ] args) {
+      int poly=(2*5*5) + (3*5) +7;
+      
+      System.out.println("The value of the second-degree poly nomial is" +poly);
+       
+     }
+   }
+         
