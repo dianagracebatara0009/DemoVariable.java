903792ddcc5652ef12819fca73b7f6e62eda98a3
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:44:38 +0800

    Create DemoVariables.java

---
+++ DemoVariables.java
@@ -0,0 +1,13 @@
+//Filename DemoVariables.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+public class DemoVariables {
+  public static void main(String[ ] args){
+  int intNo =296;
+  
+    System.out.println("The int is !");
+    System.out.println("intNo");
+  }
+}
