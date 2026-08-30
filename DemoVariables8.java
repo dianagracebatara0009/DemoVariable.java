1e978d55cd90894750caf40e5c980881dbc67a3e
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:31:17 +0800

    Create DemoVariables8.java

---
+++ DemoVariables8.java
@@ -0,0 +1,19 @@
+//Filename DemoVariable8.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+public class DemoVariables8 {
+    public static void main(String[] args) {
+        int num1 = 1, num2 = 2, num3, num4;
+
+        num3 = ++num2;
+        num4 = num1++;
+        num3++;
+
+        System.out.println("The value of num1 is " + num1);
+        System.out.println("The value of num2 is " + num2);
+        System.out.println("The value of num3 is " + num3);
+        System.out.println("The value of num4 is " + num4);
+    }
+}
