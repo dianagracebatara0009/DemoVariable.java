263ecf4ed84f5ef4a8a2cf1f636dfcf0cb774897
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:30:47 +0800

    Create DemoVariables9.java

---
+++ DemoVariables9.java
@@ -0,0 +1,14 @@
+//Filename DomoVariable9.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+public class DemoVariables9 {
+    public static void main(String[] args) {
+        char myGrade = 'A', myFriendsGrade = 'C';
+
+        System.out.println("Our grade are " + myGrade + " and " + myFriendsGrade);
+        System.out.println("\nThis is on one line \nThis is on another");
+        System.out.println("This shows \thow \ttabs \twork");
+    }
+}
