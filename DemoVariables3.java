a202600507a96c5709637224189c26ca46b0ba9d
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:43:22 +0800

    Create DemoVariables3.java

---
+++ DemoVariables3.java
@@ -0,0 +1,18 @@
+//Filename DemoVariables3.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+public class DemoVariables3 {
+	public static void main(String[ ] args) 
+	{
+		double doubNum1= 2.3, doubNum2= 14.8, doubResult;
+		
+		doubResult= doubNum1+ doubNum2;
+		System.out.println("The sum of the double is " +doubResult);
+		
+		doubResult=doubNum1+ doubNum2;
+		System.out.println(" The product of the double is " + doubResult);
+	
+ }
+}
