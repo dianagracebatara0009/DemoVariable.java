b0c11d1faad77f68b930eeae659f825748e6e29e
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:35:48 +0800

    Create DemoVariables5.java

---
+++ DemoVariables5.java
@@ -0,0 +1,18 @@
+//Filename DemoVariables5.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+package helloworld.variables;
+
+public class DemoVariables5 {
+     public static void main(String[] args)
+    {
+          int intNo = 296;
+          short shortNo = 19;
+          long longNo = 1234567876543L;
+           System.out.println("The int is " + intNo);
+           System.out.println("The short is " + shortNo);
+           System.out.println("The long is " + longNo);
+    }
+}
