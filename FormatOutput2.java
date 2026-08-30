e2cfd1813f74c4005842097c9d4040ad2dd83aea
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:19:52 +0800

    Create FormatOutput2.java

---
+++ FormatOutput2.java
@@ -0,0 +1,15 @@
+//Filename FormatOutput2.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+public class FormatOutput2 {
+    public static void main(String[] args) {
+        int no1 = 1, no2 = 23, no3 = 456, no4 = 7890;
+
+        System.out.printf("%5d\n", no1);
+        System.out.printf("%5d\n", no2);
+        System.out.printf("%5d\n", no3);
+        System.out.printf("%5d\n", no4);
+    }
+}
