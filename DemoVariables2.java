3acd8692730f1c37c70e5f179edc1d527295da0b
dianagracebatara0009 <dianagrace.v.batara@isu.edu.ph>
2026-08-30 21:44:06 +0800

    Create DemoVariables2.java

---
+++ DemoVariables2.java
@@ -0,0 +1,22 @@
+//Filename DemoVariables2.java
+//Written by Diana Grace Batara
+//BSIT NETSEC 1-2
+//Written on August 22, 2026
+
+package hellowld.variables;
+  
+ public class DemoVariables2 {
+	public static void main(String[ ] args) 
+	{
+		int intNo= 296;
+		short shortNo= 19;
+		long longNo = 1234567876543L;
+		System.out.print("The int is ");
+		System.out.println(intNo);
+		System.out.print("The short is ");
+		System.out.println(shortNo);
+		System.out.print("The long is ");
+		System.out.println(longNo);
+		
+	}
+}
